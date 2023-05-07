fe4ce0d9d87d968e67a8e25332d0058927a86bb4
admin <admin@admins-MacBook-Pro.local>
2023-05-06 20:12:03 -0500

    update

--- playbooks/git_push.sh
+++ playbooks/git_push.sh
@@ -8,6 +8,7 @@ echo "$(pwd)"
 git add .
 read -p "Enter commit message: " commit
 if [ -z "$commit" ]; then
+    echo "Commit message is empty using default message called "update""
     commit="update"
 fi
 
